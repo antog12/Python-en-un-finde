d1ccc4f63af05bfc3b382c74f11e585a0963063e
A.Ángel Abellán <72298869+antog12@users.noreply.github.com>
2023-01-31 17:32:40 +0100

    Add files via upload

---
+++ Fase_2.py
@@ -0,0 +1,9 @@
+#Cadena de texto básicos
+
+cadena= "Hola, es tiempo de software"
+print(cadena)
+
+print("----------------------")
+
+cadena2= "Hola, es tiempo de software\nEsto es una cadena\nmultilinea"
+print(cadena2)
